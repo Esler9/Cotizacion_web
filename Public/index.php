2b78f5cd21dbc8597bdcb6426bfa8b37f65cf513
Esler Escobar <escobarcarpio@gmail.com>
2025-04-17 15:49:45 -0600

    Habilitar la visualización de errores para facilitar el debug en index.php

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,12 @@
 <?php
 // public/index.php
 
-// 1. Carga automática de Composer
+// Para debug: mostrar todos los errores
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Autoload de Composer
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\SiteTypeController;
