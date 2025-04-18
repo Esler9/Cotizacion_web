de2b81a37a3029ac44f8f202804f3a72b892aab7
Esler Escobar <escobarcarpio@gmail.com>
2025-04-17 20:58:50 -0600

    Refactor CSS for options and general styles; enhance responsiveness, accessibility, and visual feedback

--- app/views/site_type.php
+++ app/views/site_type.php
@@ -2,6 +2,8 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Paso 1: Tipo de Sitio</title>
   <link rel="stylesheet" href="/assets/css/general.css">
   <link rel="stylesheet" href="/assets/css/index.css">
